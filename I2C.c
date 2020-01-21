e27fee1733f7f9f39a1f2ff04aeedbdba1d1d289
walitor <48912426+walitor@users.noreply.github.com>
2020-01-21 23:22:21 +0100

    Probably final version of I2C, fixed problems with "write" function

--- I2C.c
+++ I2C.c
@@ -1,6 +1,6 @@
 #include "I2C.h"
 
-void I2Cinit (void) 
+void I2CInit (void) 
 {
 	SIM -> SCGC4 |= SIM_SCGC4_I2C0_MASK ;
 	SIM -> SCGC5 |= (SIM_SCGC5_PORTB_MASK);
@@ -8,9 +8,9 @@ void I2Cinit (void)
 	PORTB->PCR[3] |= PORT_PCR_MUX(2); // I2C0 SCL
 	PORTB->PCR[4] |= PORT_PCR_MUX(2); // I2C0 SDA
 	I2C0->C1 &= ~(I2C_C1_IICEN_MASK);								
-	I2C0->F  |= I2C_F_MULT(2);											//mul=4
-	I2C0->F |= I2C_F_ICR(0x33);
-	I2C0->C1 |= I2C_C1_IICEN_MASK; 									//Enables I2C module operation.								//Enables I2C interrupt requests
+	I2C0->F  |= I2C_F_MULT(1);											//mul=2
+	I2C0->F |= I2C_F_ICR(0x19);
+	//I2C0->C1 |= I2C_C1_IICEN_MASK; 									//Enables I2C module operation.								//Enables I2C interrupt requests
 	I2C0->C1 |= I2C_C1_MST_MASK;										//Master Mode Select, 1-master, 0-slave
 	I2C0->C1 |= I2C_C1_TX_MASK;											//Enable transfer
 	I2C0->C1 |= I2C_C1_TXAK_MASK;										//Enable transfer acknowledge
@@ -19,15 +19,17 @@ void I2Cinit (void)
 }
 
 void I2C_write(uint8_t address, uint8_t data){
+	I2C0->C1 |= I2C_C1_IICEN_MASK;
 	I2C_TRAN;						//set to transmit mode 
 	I2C_M_START;				//send start
 	
-	I2C0->D = address;	//send write address 
+	I2C0->D = address << 1;	//send write address 
 	I2C_WAIT;						//wait for ack from salve
 	
 	I2C0->D = data;			//send data//
 	I2C_WAIT;
 	
 	I2C_M_STOP;					//set start mask off
+	I2C0->C1 &= ~(I2C_C1_IICEN_MASK);
 	return;
 }
